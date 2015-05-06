1db5561f374498fffbd080c56643faf1a346cf7e
mozooo <to.our.regret@outlook.com>
2015-05-06 11:03:41 +0900

    update script

--- js/dailybread.js
+++ js/dailybread.js
@@ -4,14 +4,9 @@ OpenSpending = "OpenSpending" in window ? OpenSpending : {};
 
 var dependentType = 'single'; // 世帯タイプ初期値
 
-// var baseKoujo = Taxes.baseKoujo; // 住民税基礎控除
-// var huyoKoujo = Taxes.huyoKoujo; // 一人分の扶養控除
-// var taxRate = Taxes.taxRate; // 住民税率
-
-// kwsk用
-var baseKoujo = DataManager.taxes.baseKoujo; // 住民税基礎控除
-var huyoKoujo = DataManager.taxes.huyoKoujo; // 一人分の扶養控除
-var taxRate = DataManager.taxes.taxRate; // 住民税率
+var baseKoujo = Taxes.baseKoujo; // 住民税基礎控除
+var huyoKoujo = Taxes.huyoKoujo; // 一人分の扶養控除
+var taxRate = Taxes.taxRate; // 住民税率
 
 var formatCurrency = function (val, prec, sym, dec, sep) {
   prec = prec === undefined ? 2 : prec
